b4acc46db45853db8c4ae942de23732078eb1a39
Nidhunkumar <nidhunkumarap@gmail.com>
2022-12-07 11:20:48 +0530

    first commit

---
+++ Evaluation_task.py
@@ -0,0 +1,25 @@
+'''Read a matrix dimension, and print the matrix filled with consecutive numbers starting from
+one(1).
+The first cell (0,0) must have the number 1.
+The last cell (n,m) must have the value n x m (The largest number).
+All others will be filled diagonally, left bottom to right top.
+For example
+Input:
+3
+4
+Output:
+1 3 6 9
+2 5 8 11
+4 7 10 12
+
+Input
+3
+3
+
+Output
+1 3 6
+2 5 8
+4 7 9
+
+
+'''
